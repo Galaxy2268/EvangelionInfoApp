854fab02a96520d6f4ffad509672e0cf5e273903
Galaxy2268 <danielsmulukinss@gmail.com>
2024-03-12 13:28:22 +0200

    initial commit

---
+++ EvangelionRestApi/src/test/kotlin/com/galaxy/evangelionrestapi/EvangelionRestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.galaxy.evangelionrestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EvangelionRestApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
