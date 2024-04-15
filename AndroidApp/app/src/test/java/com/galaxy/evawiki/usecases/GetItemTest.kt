4b16acef50ba023c77e9335b96820cef407019d8
Daniels <danielsmulukinss@gmail.com>
2024-04-15 10:27:59 +0300

    test commit

--- AndroidApp/app/src/test/java/com/galaxy/evawiki/usecases/GetItemTest.kt
+++ AndroidApp/app/src/test/java/com/galaxy/evawiki/usecases/GetItemTest.kt
@@ -1,5 +1,6 @@
 package com.galaxy.evawiki.usecases
 
+import com.galaxy.evawiki.evainfo.domain.model.Item
 import com.galaxy.evawiki.evainfo.domain.usecases.GetItem
 import com.galaxy.evawiki.evainfo.domain.usecases.util.ItemType
 import com.galaxy.evawiki.repository.FakeItemRepository
@@ -18,10 +19,22 @@ class GetItemTest {
     }
 
     @Test
-    fun `Should return either with network error`(): Unit = runBlocking{
+    fun `Should return either with network error`() = runBlocking{
         getItem = GetItem(FakeItemRepository().apply { shouldReturnNetworkError = true })
         val response = getItem(ItemType.CharacterType, 1)
         assertThat(response.isLeft()).isTrue()
     }
+
+    @Test
+    fun `Should return either with character item`(): Unit = runBlocking{
+        val response = getItem(ItemType.CharacterType, 1)
+        assertThat(response.isLeft()).isFalse()
+        val character: Item
+        response.onRight {
+            character = it
+            assertThat(character::class == Item.Character::class).isTrue()
+        }
+    }
+
 
 }
