4679b63f805d1df941280ba18d53af7ddc65dd95
Galaxy2268 <danielsmulukinss@gmail.com>
2024-04-21 15:47:33 +0300

    final changes

--- EvangelionRestApi/src/main/kotlin/com/galaxy/evangelionrestapi/domain/service/util/Path.kt
+++ EvangelionRestApi/src/main/kotlin/com/galaxy/evangelionrestapi/domain/service/util/Path.kt
@@ -1,5 +1,5 @@
 package com.galaxy.evangelionrestapi.domain.service.util
 
 object Path {
-    const val PICTURE_PATH = "http://192.168.0.148:8080/pictures/"
+    const val PICTURE_PATH = "http://192.168.203.186:8080/pictures/"
 }
